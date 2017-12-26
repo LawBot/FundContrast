5b5e4cd73684bfa8d7610dddd09bbedc8c47817c
Ben Chen <jinerben@gmail.com>
2017-12-26 09:22:31 +0100

    Refactor src package with name cn.com.xiaofabo.tylaw.fundcontrast.*

--- src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundContrast.java
+++ src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundContrast.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fundcontrast;
+package cn.com.xiaofabo.tylaw.fundcontrast.main;
 
 import java.io.FileInputStream;
 import java.io.IOException;
