d4e3c5c4c82015fedc206ecba80789d85883c898
ddl18 <ddl-15@outlook.com>
2017-12-29 13:25:39 +0100

    Added entity TextPoint

---
+++ src/cn/com/xiaofabo/tylaw/fundcontrast/entity/TextPoint.java
@@ -0,0 +1,31 @@
+package cn.com.xiaofabo.tylaw.fundcontrast.entity;
+
+/**
+ * Created @ 29.12.17
+ * by 杨敏
+ * email ddl-15 at outlook.com
+ **/
+public class TextPoint {
+    private String title;
+    private String text;
+    public TextPoint(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
