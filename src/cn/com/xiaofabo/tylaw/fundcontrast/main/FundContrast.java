d8af65f447c6739689fe278dd41ecd77f74768e0
ddl18 <ddl-15@outlook.com>
2017-12-29 16:41:09 +0100

    Updated other type of SampleDocs

--- src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundContrast.java
+++ src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundContrast.java
@@ -22,7 +22,7 @@ public class FundContrast {
      */
     public static void main(String[] args) throws IOException {
        // String inputPath = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第1号——股票型（混合型）证券投资基金基金合同填报指引（试行）.doc";
-       // String inputPath = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第2号——指数型证券投资基金基金合同填报指引（试行）.doc";
+       String inputPath = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第2号——指数型证券投资基金基金合同填报指引（试行）.doc";
         //String inputPath = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第3号——债券型证券投资基金基金合同填报指引（试行）.doc";
        // String inputPath = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第4号——货币市场基金基金合同填报指引（试行）.doc";
         StockTypeProcessor proc = new StockTypeProcessor();
