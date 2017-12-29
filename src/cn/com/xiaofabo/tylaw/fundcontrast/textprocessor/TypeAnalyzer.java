8d20751e2609dbf1046bd1325cfce1a1ffcb7d02
ddl18 <ddl-15@outlook.com>
2017-12-29 20:33:43 +0100

    analyze type of chapters and files

--- src/cn/com/xiaofabo/tylaw/fundcontrast/textprocessor/TypeAnalyzer.java
+++ src/cn/com/xiaofabo/tylaw/fundcontrast/textprocessor/TypeAnalyzer.java
@@ -6,5 +6,76 @@ package cn.com.xiaofabo.tylaw.fundcontrast.textprocessor;
  * email ddl-15 at outlook.com
  **/
 public class TypeAnalyzer {
+    public boolean isTypeOne() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isTypeTwo() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isTypeThree() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isTypeFour() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterA() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterB() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterC() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterD() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterE() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterF() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterG() {
+        boolean result = false;
+        return result;
+    }
+
+
+    public boolean isChapterH() {
+        boolean result = false;
+        return result;
+    }
+
 
 }
