667ce96804481d52f89bded54fb146816ef36fc7
ddl2018 <ddl-15@outlook.com>
2018-01-09 17:35:29 +0100

    Test type3.

--- src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundContrast.java
+++ src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundContrast.java
@@ -27,14 +27,14 @@ public class FundContrast {
     public static void main(String[] args) throws IOException, ChapterIncorrectException, SectionIncorrectException {
         String inputPath1 = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第1号——股票型（混合型）证券投资基金基金合同填报指引（试行）.doc";
         String inputPath2 = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第2号——指数型证券投资基金基金合同填报指引（试行）.doc";
-        String inputPath3 = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第3号——债券型证券投资基金基金合同填报指引（试行）.doc";
+        String inputPath3 = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第3号——证券投资基金基金合同填报指引（试行）.doc";
         String inputPath4 = "data/StandardDoc/（2012-12-17）证券投资基金基金合同填报指引第4号——货币市场基金基金合同填报指引（试行）.doc";
 
         // 工银瑞信
         //String inputPath = "data/Sample/工银瑞信/20161223工银瑞信新动力混合型/工银瑞信新动力灵活配置混合型证券投资基金基金合同（草案）－定稿20161220.docx";
         //String inputPath = "data/Sample/工银瑞信/20161229工银瑞信共享经济混合型/工银瑞信共享经济灵活配置混合型证券投资基金基金合同20161229 - 定稿.docx";
-        //String inputPath = "data/Sample/工银瑞信/20170927工银瑞信瑞祥定期开放发债券型发起式/工银瑞信瑞祥定期开放债券型发起式证券投资基金基金合同（草案）-0804.docx";
-        //String inputPath = "data/Sample/工银瑞信/20171030工银瑞信瑞景定期开放债券型/工银瑞信瑞景定期开放债券型发起式证券投资基金基金合同（草案）PD1026-clean.docx";
+        //String inputPath = "data/Sample/工银瑞信/20170927工银瑞信瑞祥定期开放发债券型发起式/工银瑞信瑞祥定期债券型开放债券型发起式证券投资基金基金合同（草案）-0804.docx";
+        String inputPathType3a = "data/Sample/工银瑞信/20171030工银瑞信瑞景定期开放债券型/工银瑞信瑞景定期开放债券型发起式证券投资基金基金合同（草案）PD1026-clean.docx";
 
         // 九泰基金
         //String inputPath = "data/Sample/九泰基金/20170413九泰天辰量化新动力混合型/九泰天辰量化新动力混合型证券投资基金合同-0224范-0228TY-0301发托管.doc";
@@ -44,15 +44,16 @@ public class FundContrast {
         //String inputPath = "data/Sample/九泰基金/20171103九泰安鑫纯债债券型/九泰安鑫纯债债券型证券投资基金合同（草案）20170725定稿清洁版.doc";
 
         // 华夏基金:债券 too many problems
-        // String inputPath = "data/Sample/华夏基金/债券/华夏鼎丰三个月定期开放债券型发起式证券投资基金基金合同20171107-托管行反馈.docx";
-        //String inputPath = "data/Sample/华夏基金/债券/华夏鼎丰三个月定期开放债券型发起式证券投资基金条文对照表 1107.docx";
-        // String inputPath = "data/Sample/华夏基金/债券/华夏鼎兴债券型证券投资基金招募说明书修改对照表-20171012.docx";
-        // String inputPath = "data/Sample/华夏基金/债券/华夏鼎康六个月定期开放债券型发起式证券投资基金条文对照表 1101.docx";
-        // String inputPath = "data/Sample/华夏基金/债券/华夏鼎康六个月定期开放债券型发起式证券投资基金基金合同20171101-托管行反馈 P18.docx";
-        //String inputPath = "data/Sample/华夏基金/债券/华夏鼎沛债券型证券投资基金基金合同.docx";
-        // String inputPath = "data/Sample/九泰基金/20171103九泰安鑫纯债债券型/九泰安鑫纯债债券型证券投资基金合同（草案）20170725定稿清洁版.doc";
+        String inputPathType3b = "data/Sample/华夏基金/债券/华夏鼎丰三个月定期开放债券型发起式证券投资基金基金合同20171107-托管行反馈.docx";
+        String inputPathType3c = "data/Sample/华夏基金/债券/华夏鼎丰三个月定期开放债券型发起式证券投资基金条文对照表 1107.docx";
+         String inputPathType3d = "data/Sample/华夏基金/债券/华夏鼎兴债券型证券投资基金招募说明书修改对照表-20171012.docx";
+         String inputPathType3e = "data/Sample/华夏基金/债券/华夏鼎康六个月定期开放债券型发起式证券投资基金条文对照表 1101.docx";
+         String inputPathType3f = "data/Sample/华夏基金/债券/华夏鼎康六个月定期开放债券型发起式证券投资基金基金合同20171101-托管行反馈 P18.docx";
+        String inputPathType3g = "data/Sample/华夏基金/债券/华夏鼎沛" +
+                "证券投资基金基金合同.docx";
+         String inputPathType3h = "data/Sample/九泰基金/20171103九泰安鑫纯债债券型/九泰安鑫纯债证券投资基金合同（草案）20170725定稿清洁版.doc";
         //华夏基金:指数
-        // String inputPath = "data/Sample/华夏基金/指数/华夏上证3-5年期中高评级可质押信用债ETF发起式联接基金基金合同20170112.doc";
+       String inputPathType2a = "data/Sample/华夏基金/指数/华夏上证3-5年期中高评级可质押信用债ETF发起式联接基金基金合同20170112.doc";
         // String inputPath = "data/Sample/华夏基金/指数/华夏中l证全指ETF合同干净版-反馈修订-TY修订 改为指数型.docx";
         // String inputPath = "data/Sample/华夏基金/指数/华夏上证3-5年期中高评级可质押信用债ETF基金合同20170109-clean.doc";
         //华夏基金:股票混合
@@ -114,8 +115,8 @@ public class FundContrast {
 //        System.out.println("*************EDN Real contract*********************");
 //        System.out.println("***************************************************");
             
-        DocProcessor dp = new DocProcessor(inputPath4);
-        dp.readText(inputPath4);
+        DocProcessor dp = new DocProcessor(inputPathType3c);
+        dp.readText(inputPathType3c);
         FundDoc fd = dp.process();
         System.out.println(fd.print());
     }
