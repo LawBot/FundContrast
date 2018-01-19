e612bcb2c611e61816caa5a962e83abc22068a70
ddl2018 <ddl-15@outlook.com>
2018-01-19 11:54:19 +0100

    Update GUI.

--- src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundGUI.java
+++ src/cn/com/xiaofabo/tylaw/fundcontrast/main/FundGUI.java
@@ -1,9 +1,10 @@
 package cn.com.xiaofabo.tylaw.fundcontrast.main;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 
 
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -16,105 +17,146 @@ import java.io.File;
  **/
 
 // An AWT program inherits from the top-level container java.awt.Frame
-public class FundGUI extends JFrame implements ActionListener {
+public class FundGUI {
 
-    private JLabel lblCom;
-    private JLabel lblType;
-    private JButton lblInput;
-    private JButton lblOutput;
-    private JButton btnSubmit;
-    private JButton btnInput;
-    private JButton btnOutput;
-    private JTextField inputPath;
-    private JTextField outputPath;
-
-    private DefaultComboBoxModel companies = new DefaultComboBoxModel();
-    private DefaultComboBoxModel type = new DefaultComboBoxModel();
 
     public FundGUI() {
-        setLayout(new FlowLayout());
-
-        lblCom = new JLabel("公司");
-        add(lblCom);
-
-        companies.addElement("工行");
-        companies.addElement("久泰");
-        companies.addElement("华夏");
-        final JComboBox companyCombo = new JComboBox(companies);
-        companyCombo.setSelectedIndex(0);
-        add(companyCombo);
-
-        lblType = new JLabel("类型");
-        add(lblType);
-        type.addElement("债券");
-        type.addElement("混合");
-        type.addElement("混合");
-        type.addElement("混合");
-        final JComboBox typeCombo = new JComboBox(type);
-        typeCombo.setSelectedIndex(0);
-        add(typeCombo);
-
-        lblInput = new JButton("选择文件");
-        add(lblInput);
-
-        inputPath = new JTextField();
-        add(inputPath);
-
-        inputPath.addActionListener(new ActionListener() {
+        EventQueue.invokeLater(new Runnable() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser Chooser = new JFileChooser();
-                Chooser.setMultiSelectionEnabled(true);
-                Chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                int result = Chooser.showDialog(FundGUI.this, "Open/Save");
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File file = Chooser.getSelectedFile();
-                    outputPath.setText(file.getAbsolutePath());
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                    ex.printStackTrace();
                 }
+
+                JFrame frame = new JFrame("生成条文");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.add(new TestPane());
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setSize(1000, 500);
+                frame.setVisible(true);
             }
         });
-
-        lblOutput = new JButton("输出路径");
-        add(lblOutput);
-        outputPath = new JTextField();
-        add(outputPath);
-        outputPath.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser Chooser = new JFileChooser();
-                Chooser.setMultiSelectionEnabled(true);
-                Chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                int result = Chooser.showDialog(FundGUI.this, "Open/Save");
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File file = Chooser.getSelectedFile();
-                    outputPath.setText(file.getAbsolutePath());
-                }
-            }
-        });
-
-        btnSubmit = new JButton("生成");
-        btnSubmit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO
-                System.out.println("generating file...");
-            }
-        });
-
-        btnSubmit = new JButton("生成条文");
-        add(btnSubmit);
-
-        setTitle("生成条文");
-        setSize(1000, 500);
-        setVisible(true);
     }
+
 
     public static void main(String[] args) {
-        FundGUI app = new FundGUI();
+        new FundGUI();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent evt) {
+    public class TestPane extends JPanel {
 
+        private JLabel lblCom;
+        private JLabel lblType;
+        private JButton lblInput;
+        private JButton lblOutput;
+        private JButton btnSubmit;
+        private JTextField inputPath;
+        private JTextField outputPath;
+        private DefaultComboBoxModel companies = new DefaultComboBoxModel();
+        private DefaultComboBoxModel type = new DefaultComboBoxModel();
+        private JFileChooser chooser;
+
+        public TestPane() {
+            setLayout(new FlowLayout());
+
+            lblCom = new JLabel("公司");
+            add(lblCom);
+
+            companies.addElement("工行");
+            companies.addElement("久泰");
+            companies.addElement("华夏");
+            final JComboBox companyCombo = new JComboBox(companies);
+            companyCombo.setSelectedIndex(0);
+            add(companyCombo);
+
+            lblType = new JLabel("类型");
+            add(lblType);
+            type.addElement("债券");
+            type.addElement("混合");
+            type.addElement("混合");
+            type.addElement("混合");
+            final JComboBox typeCombo = new JComboBox(type);
+            typeCombo.setSelectedIndex(0);
+            add(typeCombo);
+
+            inputPath = new JTextField("path...");
+            add(inputPath);
+            lblInput = new JButton("选择文件");
+            add(lblInput);
+            lblInput.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (chooser == null) {
+                        chooser = new JFileChooser();
+                        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                        chooser.setAcceptAllFileFilterUsed(false);
+                        chooser.addChoosableFileFilter(new FileFilter() {
+                            @Override
+                            public boolean accept(File f) {
+                                return f.isDirectory() || f.getName().toLowerCase().endsWith(".docx") || f.getName().toLowerCase().endsWith(".doc");
+                            }
+
+                            @Override
+                            public String getDescription() {
+                                return "Document (*.docx, *.doc)";
+                            }
+                        });
+                    }
+
+                    switch (chooser.showOpenDialog(FundGUI.TestPane.this)) {
+                        case JFileChooser.APPROVE_OPTION:
+//                            System.out.println("Input: " + chooser.getSelectedFile().getAbsolutePath());
+                            inputPath.setText(chooser.getSelectedFile().getAbsolutePath());
+                            break;
+                    }
+                }
+            });
+
+            outputPath = new JTextField("path...");
+            add(outputPath);
+            lblOutput = new JButton("输出路径");
+            add(lblOutput);
+            lblOutput.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (chooser == null) {
+                        chooser = new JFileChooser();
+                        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                        chooser.setAcceptAllFileFilterUsed(false);
+                        chooser.addChoosableFileFilter(new FileFilter() {
+                            @Override
+                            public boolean accept(File f) {
+                                return f.isDirectory() || f.getName().toLowerCase().endsWith(".docx") || f.getName().toLowerCase().endsWith(".doc");
+                            }
+
+                            @Override
+                            public String getDescription() {
+                                return "Document (*.docx, *.doc)";
+                            }
+                        });
+                    }
+
+                    switch (chooser.showOpenDialog(FundGUI.TestPane.this)) {
+                        case JFileChooser.APPROVE_OPTION:
+//                            System.out.println("Output: " + chooser.getSelectedFile().getAbsolutePath());
+                            outputPath.setText(chooser.getSelectedFile().getAbsolutePath());
+                            break;
+                    }
+                }
+            });
+
+            btnSubmit = new JButton("生成条文");
+            add(btnSubmit);
+            btnSubmit.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //TODO
+                }
+            });
+        }
     }
+
 }
